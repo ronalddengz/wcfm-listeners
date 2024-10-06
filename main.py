8bf66ad1e74b8d7f5ff900dc0cbfdae1454ab0d9
ronald dengz <64546854+ronalddengz@users.noreply.github.com>
2024-10-06 13:40:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 # Google Sheets API setup
 scopes = ["https://www.googleapis.com/auth/spreadsheets"]
-creds = Credentials.from_service_account_file("credentials.json", scopes=scopes)
+creds = Credentials.from_service_account_file("/home/runner/.config/credentials.json", scopes=scopes)  # Updated path
 client = gspread.authorize(creds)
 
 sheet_id = "1m4BktefQ3vCY3NtOhWoLc7rb9aKOMJXKR_qwAjo15oE"
@@ -16,25 +16,19 @@ def listenerCount(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     listener_div = soup.find('div', class_='card-footer d-block text-muted')
-
     if listener_div:
         listeners_text = listener_div.get_text(strip=True)
         listeners_number = listeners_text.split(' ')[0]
         return int(listeners_number)
-    
     return None
 
 def record_listeners():
     url = "http://dir.xiph.org/search?q=wcfm"
     listeners = listenerCount(url)
-
     if listeners is not None:
         print(f"Number of listeners: {listeners}")
-
         timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
         next_row = len(sheet.get_all_values()) + 1
         sheet.update(f'A{next_row}', [[listeners, timestamp]])
-    else:
-        print("Could not find the number of listeners.")
+
 record_listeners()
